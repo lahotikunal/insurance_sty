103926ff3567c1717dc04ecac52e1172d5e93dff
lahotikunal <37633212+lahotikunal@users.noreply.github.com>
2021-05-08 17:46:18 +0530

    Update lucky7java.js

--- lucky7java.js
+++ lucky7java.js
@@ -3,7 +3,7 @@ var diceValue1,
 var diceValue,
     potValue,
     radioValue = '=7',
-    bidValue = 30;
+    bidValue = 3000000;
 
 function myRadioValue(dice) {
     radioValue = dice;
@@ -140,7 +140,7 @@ button.addEventListener('click',function()
              potValue -= bidValue;
            }
         }
-  if(potValue<=45){
+  if(potValue<=4500000){
     
       pass=prompt('The insurnce contract is of worth 20 Lakhs , Do you need insurance y/n ');
       if(pass=='y'){
